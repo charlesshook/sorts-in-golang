91b3b0ff737a3bc1088a60f56b682f381bb7800c
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2020-09-25 12:13:48 -0500

    Created quicksort function

--- quicksort_test.go
+++ quicksort_test.go
@@ -7,6 +7,7 @@ import (
 
 func Test_QuickSort(t * testing.T) {
 	taco := CreateAndFillSliceWithRandomInts(100)
+	QuickSort(&taco, 0, len(taco) -1)
 
 	if !sort.SliceIsSorted(taco, func(p, q int) bool {  
 		return taco[p] < taco[q] }) {
@@ -14,12 +15,16 @@ func Test_QuickSort(t * testing.T) {
 	}
 
 	taco = CreateAndFillSliceWithRandomInts(1000)
+	QuickSort(&taco, 0, len(taco) -1)
+
 	if !sort.SliceIsSorted(taco, func(p, q int) bool {  
 		return taco[p] < taco[q] }) {
 		t.Error("Slice with 1000 elements not sorted")
 	}
 
 	taco = CreateAndFillSliceWithRandomInts(10000)
+	QuickSort(&taco, 0, len(taco) -1)
+
 	if !sort.SliceIsSorted(taco, func(p, q int) bool {  
 		return taco[p] < taco[q] }) {
 		t.Error("Slice with 10000 elements not sorted")
